2cb544b2a0030375b87cf3efaa083af5a963c0d0
nejd <nejdbedoui@gmail.com>
2020-11-15 05:49:12 +0100

    project initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListmaterielComponent } from './listmateriel/listmateriel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListmaterielComponent
   ],
   imports: [
     BrowserModule
